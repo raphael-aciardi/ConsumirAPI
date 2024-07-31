c10e22618b7aaa6a8c98510a15d6035dfde7cfb7
raphael-aciardi <996193248ph@gmail.com>
2024-07-31 19:27:19 -0300

    Conxeao banco

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from pokmemon import PokemonData
+import mysql.connector
 import requests
+from ConectaBanco import ConexãoBanco
+
+banco = ConexãoBanco()
 
 urls = {
     "pokemon": "https://pokeapi.co/api/v2/pokemon/",
@@ -8,15 +11,33 @@ urls = {
     "BatleStyle" : "https://pokeapi.co/api/v2/move-battle-style"
 }
 
-pokelist = []
 
-for key, url in urls.items():
+def AcessarJson():
+    
+    for key, url in urls.items():
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Conexão realizada com a url {url}")
+            else:
+                print(key, "NOT OK")
+        except requests.RequestException as e:
+            print("Failed to use the URL:", url, "\n\nError:", e)
+
+def startConnection():
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(key, "OK")
-        else:
-            print(key, "NOT OK")
-    except requests.RequestException as e:
-        print("Failed to use the URL:", url, "\n\nError:", e)
+        mydb = mysql.connector.connect(
+            host = banco.host,
+            user = banco.user,
+            password = banco.password,
+            database = banco.database
+            )
+        
+        mydb.cursor()
+        
+    except Exception as e:
+        print("Não foi possível se conectar, erro: ", e)
 
+
+startConnection()
+AcessarJson()
