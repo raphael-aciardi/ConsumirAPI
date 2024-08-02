fe0901dad44636e6cf14a22f00e273a70244e7a8
raphael-aciardi <996193248ph@gmail.com>
2024-08-01 22:07:48 -0300

    Inserindo nomes do pokemon no Mysql

--- main.py
+++ main.py
@@ -2,42 +2,61 @@ import mysql.connector
 import requests
 from ConectaBanco import ConexãoBanco
 
+
 banco = ConexãoBanco()
+
 
 urls = {
     "pokemon": "https://pokeapi.co/api/v2/pokemon/",
-    "evolution": "https://pokeapi.co/api/v2/evolution-chain/",
     "move": "https://pokeapi.co/api/v2/move/",
-    "BatleStyle" : "https://pokeapi.co/api/v2/move-battle-style"
 }
 
 
-def AcessarJson():
-    
-    for key, url in urls.items():
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print(f"Conexão realizada com a url {url}")
-            else:
-                print(key, "NOT OK")
-        except requests.RequestException as e:
-            print("Failed to use the URL:", url, "\n\nError:", e)
+pokelist = []
+movelist = []
 
-def startConnection():
+
+for key, url in urls.items():
     try:
-        mydb = mysql.connector.connect(
-            host = banco.host,
-            user = banco.user,
-            password = banco.password,
-            database = banco.database
-            )
-        
-        mydb.cursor()
-        
-    except Exception as e:
-        print("Não foi possível se conectar, erro: ", e)
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Conexão realizada com a URL {url}")
+            if key == "pokemon":
+                for pokemon in response.json()["results"]:
+                    pokelist.append(pokemon["name"])  
+            elif key == "move":
+                for move in response.json()["results"]:
+                    movelist.append(move["name"])
+        else:
+            print(key, "NOT OK")
+    except requests.RequestException as e:
+        print("Failed to use the URL:", url, "\n\nError:", e)
 
 
-startConnection()
-AcessarJson()
+try:
+    conexao = mysql.connector.connect(
+        host=banco.host,
+        user=banco.user,
+        password=banco.password,
+        database=banco.database
+    )
+    
+    conexao.autocommit = False
+    cursor = conexao.cursor()
+    
+    for pokemon in pokelist:
+        select = "INSERT INTO pokemon.t_pokemon (nome) VALUES (%s);"
+        cursor.execute(select, (pokemon,))
+        print(f"Inserted {pokemon}")
+
+    conexao.commit()
+    
+except mysql.connector.Error as e:
+    print("Não foi possível se conectar, erro: ", e)
+    conexao.rollback()
+
+finally:
+    if 'conexao' in locals() and conexao.is_connected():
+        cursor.close()
+        conexao.close()
+        print("Conexão com o banco de dados encerrada.")
